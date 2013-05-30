989e21c34a8ff911fd33e19bc44eca718b37632c
Jesús Martínez <jesus.martinez@synergy-gb.com>
2013-05-30 16:27:06 -0430

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BmCm::Application.config.secret_token = 'd20f6c0ee0f95f231e3785b10f9b67dd8fe5162602192b29a090691a48a97fa542668146444ab666aac7518601c62a412d712a5444803c362b6934d7ae013d02'
